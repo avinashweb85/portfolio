8d2f271ffaef6c5dabf5478d824ec42fafba9e5c
avinashweb85 <avis.8586@gmail.com>
2024-03-24 21:55:22 +0530

    resume section

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -9,58 +9,35 @@ const Education = () => {
       animate={{ opacity: 1, transition: { duration: 0.5 } }}
       className="w-full flex flex-col lgl:flex-row gap-10 lgl:gap-20"
     >
-      {/* part one */}
-      <div>
+      <div className='w-full'>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">1998 - 2010</p>
-          <h2 className="text-3xl md:text-4xl font-bold">Education Quality</h2>
-        </div>
-        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
-          <ResumeCard
-            title="BSc in Computer Science"
-            subTitle="University of DVI (2006 - 2010)"
-            result="3.90/4"
-            des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
-          />
-          <ResumeCard
-            title="AS - Science & Information"
-            subTitle="SuperKing College (2001 - 2005)"
-            result="4.75/5"
-            des="Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
-          />
-          <ResumeCard
-            title="Secondary School Education"
-            subTitle="Kingstar Secondary School (1998 - 2000)"
-            result="5.00/5"
-            des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
-          />
-        </div>
-      </div>
-      {/* part Two */}
-
-      <div>
-        <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2010 - 2022</p>
+          <p className="text-sm text-designColor tracking-[4px]">2019 - Current</p>
           <h2 className="text-3xl md:text-4xl font-bold">Job Experience</h2>
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
             title="Sr. Software Engineer"
-            subTitle="Google Out Tech - (2017 - Present)"
-            result="USA"
-            des="Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up."
+            subTitle="GlobalLogic - (January 2023 - Present)"
+            result="Noida, India"
+            des="GlobalLogic is a known digital product engineering company in India that offers complete digital transformation solutions & software engineering services."
           />
           <ResumeCard
-            title="Web Developer & Trainer"
-            subTitle="Apple Developer Team - (2012 - 2016)"
-            result="MALAYSIA"
-            des="A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a role in Malaysia isn't easy."
+            title="Software Engineer"
+            subTitle="Zimyo - (February 2023 - November 2023)"
+            result="Gurugram, India"
+            des="Zimyo is a best HR Software for businesses of all sizes. Zimyo HCM helps organizations automate their HR, payroll, performance, recruitment, & everything HR."
           />
           <ResumeCard
             title="Front-end Developer"
-            subTitle="Nike - (2020 - 2011)"
-            result="Oman"
-            des="The Oman economy has grown strongly over recent years, having transformed itself from a producer and innovation-based economy."
+            subTitle="Drife - (August 2022 - December 2022)"
+            result="Bangalore, India"
+            des="DRIFE is a blockchain-based mobility platform that seeks to decentralize the ride-hailing ecosystem. 100% of all fees go back to the driver."
+          />
+          <ResumeCard
+            title="Web Developer"
+            subTitle="Fox Consultings - (July 2019 - August 2022)"
+            result="Noida, India"
+            des="Fox Consultings LLC: Connecting Talent with Opportunity We specialize in bridging the gap between exceptional talent and promising career opportunities."
           />
         </div>
       </div>
--- src/data/data.js
+++ src/data/data.js
@@ -8,36 +8,24 @@ export const featuresData = [
   {
     id: 1,
     title: "Business Stratagy",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    des: "Leverage frontend development to align digital solutions with business goals, emphasizing user-centric design to drive engagement and foster growth within dynamic market landscapes.",
   },
   {
     id: 2,
     icon: <AiFillAppstore />,
     title: "App Development",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    des: "Specialize in creating captivating frontend interfaces for apps, focusing on seamless user experiences through innovative design and technology integration.",
   },
   {
     id: 3,
     icon: <SiProgress />,
     title: "SEO Optimisation",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
-  },
-  {
-    id: 4,
-    icon: <FaMobile />,
-    title: "Mobile Development",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
-  },
-  {
-    id: 5,
-    icon: <SiAntdesign />,
-    title: "UX Design",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    des: "Integrate SEO best practices into frontend development to enhance website visibility and accessibility, ensuring optimal user experience and search engine ranking.",
   },
   {
     id: 6,
     icon: <FaGlobe />,
     title: "Hosting Websites",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    des: "Manage website hosting infrastructure, ensuring reliable performance and security while optimizing frontend code for efficient delivery and seamless user experiences.",
   },
 ];
